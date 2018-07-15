1ad9b63d39a8995a399f033071bcadf437378314
Arti Kearney <artemis.wk@gmail.com>
2018-07-15 13:23:52 -0700

    Simple plugin to fix the endless vibration bug

---
+++ VibrationFix/VibrationFixPlugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IllusionPlugin;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VibrationFix
+{
+    public class VibrationFixPlugin : IPlugin
+    {
+        public string Name => "Vibration Fix Plugin";
+
+        public string Version => "1.0.0";
+        public void OnApplicationStart()
+        {
+            SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        }
+
+        private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            if (arg0.buildIndex == 4) // SongLevel
+            {
+                Console.WriteLine("Loaded SongLevel");
+                GameObject go = new GameObject("Vibration Fixer");
+                VibrationFixer injector = go.AddComponent<VibrationFixer>();
+            }
+        }
+
+        public void OnApplicationQuit()
+        {
+            SceneManager.activeSceneChanged -= SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+    }
+}
---
+++ VibrationFix/VibrationFixer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using ReflectionUtilEnhanced;
+using UnityEngine.XR;
+
+namespace VibrationFix
+{
+    public class VibrationFixer : MonoBehaviour
+    {
+        public ObstacleSaberSparkleEffectManager effectManager;
+        public HapticFeedbackController haptics;
+        public Saber left;
+        public void Update()
+        {
+            // Look for the effect manager every tick until we find it
+            if (effectManager == null)
+            {
+                effectManager = UnityEngine.Object.FindObjectOfType<ObstacleSaberSparkleEffectManager>();
+                if (effectManager != null)
+                {
+                    foundEffectManager();
+                }
+            }
+        }
+
+        public void foundEffectManager()
+        {
+            effectManager.sparkleEffectDidStartEvent += onSparkleEffectStart;
+            effectManager.sparkleEffectDidEndEvent += onSparkleEffectEnd;
+            haptics = effectManager.getPrivateField<HapticFeedbackController>("_hapticFeedbackController");
+            left = effectManager.getPrivateField<Saber[]>("_sabers")[0];
+        }
+
+        public void onSparkleEffectStart(Saber.SaberType type) { }
+
+        public void onSparkleEffectEnd(Saber.SaberType type)
+        {
+            if (haptics != null)
+            {
+                // Robust against left being either A or B. Not robust against left and right being the same type.
+                // This is because the effect manager's exposed events only tell us what type of saber stopped vibrating,
+                // not whether it was left or right.
+                if (type == left.saberType)
+                {
+                    haptics.UpdateStartCount(XRNode.LeftHand,
+                        haptics.getPrivateField<int>("_leftControllerStartRumbleCount") * -1);
+                }
+                else
+                {
+                    haptics.UpdateStartCount(XRNode.RightHand,
+                        haptics.getPrivateField<int>("_rightControllerStartRumbleCount") * -1);
+                }
+            }
+        }
+
+        public void OnDisable()
+        {
+            if (effectManager != null)
+            {
+                effectManager.sparkleEffectDidStartEvent -= onSparkleEffectStart;
+                effectManager.sparkleEffectDidEndEvent -= onSparkleEffectEnd;
+                effectManager = null;
+                haptics = null;
+            }
+        }
+    }
+}
